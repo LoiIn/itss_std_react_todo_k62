ed64dd144f7e9f85de0718a45dd3ab29f43b8836
EC2 Default User <ec2-user@ip-172-31-69-202.ec2.internal>
2021-05-21 07:58:38 +0000

    Login by Authentication

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDlnxfONKxqJWVUTD08QaLDncJgV-sc-EY",
-    authDomain: "fir-sample-8d599.firebaseapp.com",
-    projectId: "fir-sample-8d599",
-    storageBucket: "fir-sample-8d599.appspot.com",
-    messagingSenderId: "877692137874",
-    appId: "1:877692137874:web:4d25ab4c4e85cd1733d14b"
+    apiKey: "AIzaSyAhRLYe9lC6g3YMXviyMjHnKfpYLv9MFs0",
+    authDomain: "fir-sample-30792.firebaseapp.com",
+    projectId: "fir-sample-30792",
+    storageBucket: "fir-sample-30792.appspot.com",
+    messagingSenderId: "616237505675",
+    appId: "1:616237505675:web:46229516aa53373163088e"
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -14,6 +14,43 @@ firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 export const auth = firebase.auth();
 export default firebase;
+
+// login with auth
+export const uiConfig = {
+  signInFlow: 'popup',
+  signInSuccessUrl: "/",
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+  ],
+}
+
+export const storeUserInfo = async (user) => {
+  const { uid } = user;
+  const userDoc = await db.collection("users").doc(uid).get();
+  if (!userDoc.exists) {
+    await db.collection("users").doc(uid).set({ name: user.displayName });
+    return {
+      name: user.displayName,
+      id: uid,
+    };
+  } else {
+    return {
+      id: uid,
+      ...userDoc.data(),
+    };
+  }
+}
+
+export const updateUser = async (user, image) => {
+  try {
+    const userDoc = await firebase.firestore().collection("users").doc(user.id).get();
+    if (userDoc.exists) {
+      await firebase.firestore().collection("users").doc(user.id).update({ ...userDoc.data(), image: image });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+} 
 
 export const getItemsFromFirebase = async () => {
     try{
